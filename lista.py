d4ccfea1e9fd11ada3d083016827a41fa2e85cd8
Murilo Fischer <mufischer@hotmail.com>
2024-07-01 11:18:14 -0300

    add lista e tuplas

---
+++ lista.py
@@ -0,0 +1,8 @@
+frutas = ['laranja', 'maçã', 'uva']
+print(frutas[0])
+print(frutas[-1])
+frutas= []
+letras = list('python')
+numeros = list(range(10))
+palavra = 'olar'
+print(palavra[::-1] + f', {letras}, {numeros}' )
---
+++ tuplas.py
@@ -0,0 +1,4 @@
+fruta = ('laranja', 'pera', 'uva',)
+letras = tuple('python')
+numeros = tuple([1,2,3,4])
+pais = ('Brasil',)
